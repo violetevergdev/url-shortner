e2df7b762aed4768b710fff9dc4cec0d5e55b0c7
VioletEverg <belabelllil@gmail.com>
2024-05-01 21:27:03 +0300

    first commit

---
+++ cmd/url-shortner/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// TODO: init config:cleanenv
+
+	// TODO: init logger:log/slog
+
+	// TODO: init storage:sqllite
+
+	// TODO: init router:chi
+
+	// TODO: run server
+
+}
